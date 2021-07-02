3af6401c97afe453ded683d6b48f90556ccb52d8
d855 <dosmokrovic@yahoo.com>
2021-07-02 14:36:11 +0200

    nick-cage-room

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+var server = require('http').createServer(app)
+var io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    socket.on('message', (data) => {
+        console.log(data);
+        io.emit('newCords', data)
+    })
+
+    socket.on('disconnect', function() {
+        console.log('User disconnected')
+    })
+})
+
+server.listen(3000, function(){
+    console.log('Server is running');
+})
